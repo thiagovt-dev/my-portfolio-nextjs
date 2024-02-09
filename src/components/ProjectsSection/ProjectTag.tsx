b8d662ee198d6af052a62be6a6f5a6ebc60bd9e4
Thiago <thiagovt.dev@gmail.com>
2024-02-09 10:37:57 -0300

    edit projects section

--- src/components/ProjectsSection/ProjectTag.tsx
+++ src/components/ProjectsSection/ProjectTag.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
 interface ProjectTagProps {
-    name:string,
-    onClick: ()=> void,
-    isSelected:boolean
+  name: string;
+  onClick: () => void;
+  isSelected: boolean;
 }
 
-const ProjectTag = ({name, onClick, isSelected}: ProjectTagProps) => {
-  const buttonStyles = isSelected ? "border-blue-500" : "text-[#ADB7BE] border-slate-600 hover:border-white"
+const ProjectTag = ({ name, onClick, isSelected }: ProjectTagProps) => {
+  const buttonStyles = isSelected
+    ? "border-blue-500"
+    : "text-[#ADB7BE] border-slate-600 hover:border-white";
   return (
-    <button className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`} onClick={onClick}>
+    <button
+      className={`${buttonStyles} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+      onClick={onClick}>
       {name}
     </button>
   );
-}
+};
 
-export default ProjectTag
+export default ProjectTag;
