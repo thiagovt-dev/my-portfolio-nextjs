a5d83ce47376acce24dd01e06843391f8ef5d5a9
Thiago <thiagovt.dev@gmail.com>
2024-02-09 10:34:47 -0300

    create email template with react email

--- src/components/EmailSection/EmailSection.tsx
+++ src/components/EmailSection/EmailSection.tsx
@@ -33,7 +33,7 @@ const EmailSection = () => {
 
     if (response.status === 200) {
       setEmailSubmitted(true);
-      setButtonText("Enviar Mensagem")
+      setButtonText("Enviar Mensagem");
       setFormData({
         name: "",
         email: "",
@@ -46,7 +46,7 @@ const EmailSection = () => {
     }
   };
   return (
-    <section className="grid md:grid-cols-2 my-12 py-12 gap-4 relative">
+    <section className="grid md:grid-cols-2 my-12 py-12 gap-4 relative" id="contact">
       <div className="z-10">
         <h5 className="text-xl font-bold my-2">Vamos Conversar?</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
@@ -68,7 +68,7 @@ const EmailSection = () => {
               <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
             </svg>
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/thiagovasconcelosteixeira/">
             <svg
               className="h-6 w-6 text-white-500"
               viewBox="0 0 24 24"
@@ -85,7 +85,7 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        <form className="flex flex-col" onSubmit={(event)=>handleSubmit(event)}>
+        <form className="flex flex-col" onSubmit={(event) => handleSubmit(event)}>
           <div className="mb-6">
             <label htmlFor="name" className="block mb-2 text-sm font-medium">
               Seu nome
@@ -148,7 +148,7 @@ const EmailSection = () => {
           <button
             type="submit"
             className="bg-blue-500 hover:bg-blue-800 font-medium py-2.5 px-5 rounded-lg w-full flex justify-center ">
-              {buttonText}
+            {buttonText}
           </button>
         </form>
         {emailSubmitted && (
--- src/components/EmailSection/EmailTemplate.tsx
+++ src/components/EmailSection/EmailTemplate.tsx
@@ -1,3 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
+import {
+  Body,
+  Button,
+  Container,
+  Column,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Text,
+} from "@react-email/components";
+import { Tailwind } from "@react-email/tailwind";
 import * as React from "react";
 
 interface EmailTemplateProps {
@@ -6,13 +24,95 @@ interface EmailTemplateProps {
   message: string;
 }
 
-export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({name, subject, message }) => (
-  <div>
-    <h1>{subject}</h1>
-    <p>Obrigado pelo seu contato, {name}!</p>
-    <p>Nova mensagem enviada:</p>
-    <p>{message}</p>
-  </div>
-);
+export const EmailTemplate = ({ name, subject, message }: EmailTemplateProps) => {
+  const previewText = `Que bom ter você aqui, ${name}!`;
+
+  return (
+    <Html>
+      <Head />
+      <Preview>{previewText}</Preview>
+      <Tailwind>
+        <Body className="bg-white my-auto mx-auto font-sans px-2">
+          <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto p-[20px] max-w-[580px]">
+            <Section className="mt-[32px]">
+              <Img
+                src={`${process.env.NEXT_PUBLIC_API_URL}/reshot-icon-browser-DEAK34BLPC.svg`}
+                width="40"
+                height="37"
+                alt="logo"
+                className="my-0 mx-auto"
+              />
+            </Section>
+            <Heading className="text-black text-[28px] font-normal text-center p-0 my-[30px] mx-0">
+              Obrigado pelo seu contato, <strong>{name}</strong>!
+            </Heading>
+            <Text className="text-black text-[16px] px-6 mb-10 leading-[24px]">
+              É um prazer receber sua mensagem:
+            </Text>
+            <Section className="text-black text-[14px] rounded-xl mx-auto max-w-[90%] bg-[#f8f8f8] shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] leading-[24px]">
+              <Row className="flex pt-6 pb-0 px-6 ">
+                <Column align="left" className="block mr-3">
+                  <strong>Assunto: </strong>
+                </Column>
+                <Column align="left">{subject}</Column>
+              </Row>
+              <Text className="pt-0 px-6 pb-6 leading-6">"{message}"</Text>
+            </Section>
+            <Text className="text-black text-[16px] px-6 leading-[24px] mt-10 text-justify">
+              Estou empenhado em fornecer uma resposta rápida e eficiente ao seu contato. Em breve,
+              você receberá um e-mail com uma proposta personalizada que certamente atenderá às suas
+              expectativas e necessidades.
+            </Text>
+            <Text className="text-black text-[16px] px-6 leading-[24px] text-center mt-10">
+              Enquanto isso, fique a vontade para conferir minhas redes profissionais
+            </Text>
+
+            <Section className="flex justify-center items-center ">
+              <Row className="flex">
+                <Column className="block mr-2">
+                  <Link
+                    className="flex justify-center items-center gap-1"
+                    href="https://github.com/thiagovt-dev">
+                    <Text className="underline">Acesse meu portfolio no github</Text>
+                    <Img
+                      src={`${process.env.NEXT_PUBLIC_API_URL}/icons8-github.svg`}
+                      width="24"
+                      height="24"
+                      alt="Vercel"
+                      className="my-0 mx-auto"
+                    />
+                  </Link>
+                </Column>
+                <Column>
+                  <Link
+                    className="flex items-center justify-center gap-1"
+                    href="https://www.linkedin.com/in/thiagovasconcelosteixeira/">
+                    <Text className="underline">Acesse meu perfil no linkedin </Text>
+                    <Img
+                      src={`${process.env.NEXT_PUBLIC_API_URL}/icons8-linkedin.svg`}
+                      width="24"
+                      height="24"
+                      alt="Vercel"
+                      className="my-0 mx-auto"
+                    />
+                  </Link>
+                </Column>
+              </Row>
+            </Section>
+            <Hr className="border border-solid border-[#cccccc] my-[20px] mx-0 w-full" />
+            <Section className="text-center mb-5">
+              <Text className="text-[#9ca299] text-[14px] leading-6 m-0 ">
+                Thiago Vasconcelos - Desenvolvedor Fullstack
+              </Text>
+              <Text className="text-[#9ca299] text-[14px] m-0 leading-6">
+                Imperatriz-Ma, Brasil, 65900-000
+              </Text>
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
 
 export default EmailTemplate;
